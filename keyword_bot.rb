3185796ba25a957eecdb71614db16db4c8800c4d
Gordon DuQuesnay <gduquesnay@gmail.com>
2016-04-08 15:22:02 -0400

    Removed spurious prompt

--- keyword_bot.rb
+++ keyword_bot.rb
@@ -16,11 +16,24 @@ prompts <<  ENV['KEYWORD_7']
 prompts <<  ENV['KEYWORD_8']
 prompts <<  ENV['KEYWORD_9']
 
+matched = false
+
 prompts.each do |p|
   unless p.nil?
     if keyword(p) == initialMsg
+      matched = true
       puts p.split(":").last
     end
  end
+end
+
+unless matched
+  choices = ""
+  prompts.each do |p|
+    unless p.nil?
+      choices += keyword(p) + " - "
+    end
+  end
+  puts "Sorry, I don't know that keyword. Available choice are #{choices}"
 end
 
